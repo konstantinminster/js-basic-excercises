0f37622a9ddf79595d3f6e46c21637d6303ab6e3
konstantinminster <konstantin@minster.biz>
2024-05-28 13:33:29 +0200

    finished task #2

---
+++ 2.js
@@ -0,0 +1,24 @@
+// You can work here or download the template!
+
+let nummer = 55;
+let numAsString = '10';
+console.log(nummer + numAsString);
+// takes the 55 from nummer and adds 55 after since numAsString is a string
+
+console.log(nummer - numAsString);
+// because JS is doing strange things :D
+
+console.log(numAsString * nummer);
+
+console.log(nummer / numAsString);
+
+let num1 = 6;
+let num2 = '4';
+
+console.log(num1 % num2);
+
+let num3 = 3;
+let num4 = 10;
+let num5 = 7;
+
+console.log(num1 + num3 + num2 * num4);
