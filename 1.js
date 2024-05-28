71d0e6eece12f23e7f5fb791a4071f2e3970e4d9
konstantinminster <konstantin@minster.biz>
2024-05-28 13:21:28 +0200

    finished task #1

---
+++ 1.js
@@ -0,0 +1,14 @@
+// You can work here or download the template!
+let age = 25;
+const birthYear = 1999;
+let name = 'John Doe';
+const isStudent = true;
+let favoriteColor = 'blue';
+
+age = 26;
+name = 'Jane Doe';
+//birthYear = 2000;
+console.log(age, birthYear, name, isStudent);
+console.log(favoriteColor);
+favoriteColor = 'green';
+console.log(favoriteColor);
